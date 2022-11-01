0db5c309809d2132e26eed9db9fac11eb25ae36b
Misha Tuesday <mishatuesday@gmail.com>
2022-10-31 21:51:51 -0400

    commit tree 5f1ad2c5a687e58e9c1a60ecabbe78a7fb56d306

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+snoopy = Dog.new
+snoopy.bark
+misha = Person.new
+misha.talk
+misha.walk
